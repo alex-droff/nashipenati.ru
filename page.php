0c339cc7d5fdd83d85751a4b4878d7fe482be31f
Alex Droff <alex.droff@gmail.com>
2022-04-13 19:52:03 +0300

    Скроем виджеты фейсбука в сайдбаре

--- page.php
+++ page.php
@@ -189,9 +189,10 @@ require_once "topfile-head.php";
 										<? } ?>
 									</ul>
 
-								</div><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fnashipenati%2F&tabs=timeline&width=270&height=350&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="270" height="350" style="border:none;overflow:hidden"></iframe>
+									</div><? /*?><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fnashipenati%2F&tabs=timeline&width=270&height=350&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="270" height="350" style="border:none;overflow:hidden"></iframe>
 							</div>
-							<div><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcitrusmontessorischool%2F&tabs=timeline&width=270&height=350&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="270" height="350" style="border:none;overflow:hidden"></iframe></div>
+							<div><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcitrusmontessorischool%2F&tabs=timeline&width=270&height=350&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="270" height="350" style="border:none;overflow:hidden"></iframe><? */?>
+							</div>
 
 						</div>
 					</div>
